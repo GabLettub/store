63b31dd249a18501096e4a3f3536a38d073cbba4
Angel Gabriel Gabayan Litob Jr <agglitobjr@addunet.ph>
2024-09-17 18:35:21 +0800

    Intialize React for Git

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div>
+      <input type="text" />
+      <input type="checkbox" />
+      Only show products in stock
+    </div>
+  );
+};
+
+export default SearchBar;
