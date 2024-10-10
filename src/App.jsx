451ed1dc9d78953748b0d37defdd65f819d7468d
GabLettub <agglitobjr@addu.edu.ph>
2024-10-10 19:27:56 +0800

    using fakestore API, using tailwind css, new search bar visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FilterProductTable from "./components/FilterProductTable";
-import ProductTable from "./components/ProductTable";
 import SearchBar from "./components/SearchBar";
 
-const products = [
-  //sporting goods
-  { id: 1, name: "Tennis", price: 99.9, type: 1, stock: 100 },
-  { id: 2, name: "Badminton", price: 99.9, type: 1, stock: 400 },
-  { id: 3, name: "Basketball", price: 99.9, type: 1, stock: 700 },
-
-  //electronics
-  { id: 4, name: "Charger", price: 67.123123, type: 2, stock: 1200 },
-  { id: 5, name: "Tablet", price: 88.9, type: 2, stock: 170 },
-  { id: 6, name: "Smart Phone", price: -100.9, type: 2, stock: 160 },
-];
-
-const headers = ["Sporting Goods", "Electronics"];
-
 function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true); // Set to true initially
   const [query, setQuery] = useState("");
   const [stockChecked, setStockChecked] = useState(false);
 
   const filteredProducts = products.filter(
     (product) =>
-      product.name.toLowerCase().includes(query.toLowerCase()) &&
-      //Case 2: If it has stocks
-      // !stockChecks: if the valie is false it can be considered
-      // produc.stock > 0 we should render stocks that are not zero
-      // || because either of the case is needed
+      product.title.toLowerCase().includes(query.toLowerCase()) && // Use title instead of name
       (!stockChecked || product.stock > 0)
   );
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products/");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      } finally {
+        setLoading(false); // Set loading to false after the fetch is done
+      }
+    };
+    getProducts();
+  }, []);
 
   return (
     <FilterProductTable>
@@ -40,10 +38,19 @@ function App() {
         stockChecked={stockChecked}
         setStockChecked={setStockChecked}
       />
-      <ProductTable
-        headers={headers}
-        products={filteredProducts}
-      ></ProductTable>
+      {!loading ? (
+        <div className="flex flex-col gap-4">
+          {filteredProducts.map(
+            (
+              product // Use filteredProducts here
+            ) => (
+              <span key={product.id}>{product.title}</span>
+            )
+          )}
+        </div>
+      ) : (
+        <span>Loading...</span>
+      )}
     </FilterProductTable>
   );
 }
